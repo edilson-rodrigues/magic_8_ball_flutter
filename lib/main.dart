a1b878de4e5c16824cced2184d22c0270978d5b7
Edilson Rodrigues <edarodrigs@gmail.com>
2020-11-21 17:52:11 -0300

    randomise it

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(home: BallPage()),
     );
 
-class BallPage extends StatefulWidget {
-  @override
-  _BallPageState createState() => _BallPageState();
-}
-
-class _BallPageState extends State<BallPage> {
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text(
-          'Ask Me Anything',
-        ),
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
       ),
-      body: Container(
-        child: Center(
-          child: FlatButton(
-            onPressed: () {
-              print('I got cliked');
-            },
-            child: Image.asset(
-              'images/ball1.png',
-            ),
-          ),
-        ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          ballNumber = Random().nextInt(5);
+          print(ballNumber);
+        },
+        child: Image.asset('images/ball1.png'),
       ),
     );
   }
