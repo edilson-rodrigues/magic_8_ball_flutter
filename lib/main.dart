f515e4681fa126bf4e0dd3dcfa5e18b988472f6d
Edilson Rodrigues <edarodrigs@gmail.com>
2020-11-21 17:36:40 -0300

    created the android and ios structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Ask Me Anything',
+            ),
+          ),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
